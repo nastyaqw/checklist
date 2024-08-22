79c7efbe5cbe0398a16615894e74b5752a6fe504
nastyaqw <79269539+nastyaqw@users.noreply.github.com>
2024-08-22 23:30:00 +0400

    t

--- app/Http/Controllers/ChecklistController.php
+++ app/Http/Controllers/ChecklistController.php
@@ -14,6 +14,7 @@ class ChecklistController extends Controller
     /**
      * Instantiate a new ChecklistController instance.
      */
+    
     public function __construct()
     {
        $this->middleware('auth');
